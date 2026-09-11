c2c46c06bcd87023e00942b60c7ad36445438494
Keith Pereira <keithcpereira@gmail.com>
2026-09-11 15:21:28 +0530

    feat: Basic DFS Ordering

--- src/Dom.cpp
+++ src/Dom.cpp
@@ -3,6 +3,12 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/CFG.h"
+
+#include <cstddef>
+#include <vector>
+#include <set>
 
 // using namespace llvm;
 
@@ -14,20 +20,40 @@ namespace {
         llvm::FunctionAnalysisManager &
         ) {
 
-      llvm::errs() << "Function: "
-        << F.getName() << "\n";
-
-      for (llvm::BasicBlock &BB : F) {
-        // Iterate over each Instruction in the BasicBlock
-        BB.print(llvm::errs());
-        for (llvm::Instruction &I : BB) {
-          // Print the instruction as a line of IR
-          I.print(llvm::errs());
-          llvm::errs() << "\n";
-        }
+      auto ordering = get_dfs_ordering(F);
+      
+      int i = 0;
+      for (auto BB : ordering){
+        llvm::errs() << i << ": " << BB->getName() << ", ";
+        i++;
       }
 
       return llvm::PreservedAnalyses::all();
+    }
+
+    std::vector<const llvm::BasicBlock*> get_dfs_ordering(const llvm::Function &F) {
+      std::size_t no_of_BB = F.size();
+      std::set<const llvm::BasicBlock*> visited;
+      std::vector<const llvm::BasicBlock*> ordering;  
+      ordering.reserve(no_of_BB);
+
+      dfsRec(visited, F.getEntryBlock(), ordering);
+
+      return (ordering);
+    }
+
+    void dfsRec(std::set<const llvm::BasicBlock*> &visited, const llvm::BasicBlock &BB, std::vector<const llvm::BasicBlock*> &ordering) {
+      llvm::errs() << "Visiting: " << BB.getName() << "\n";
+      
+      visited.insert(&BB);
+
+      ordering.push_back(&BB);
+
+      // Recursively visit all adjacent vertices
+      // that are not visited yet
+      for (const llvm::BasicBlock *Succ: llvm::successors(&BB))
+        if (visited.find(Succ) == visited.end())
+          dfsRec(visited, *Succ, ordering);
     }
   };
 
