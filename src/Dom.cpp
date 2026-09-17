8dcce4e25511a88ada6b3fb49c097afecbce7aea
Keith Pereira <keithcpereira@gmail.com>
2026-09-18 03:06:37 +0530

    fix: changed variable names
    STILL DOES NOT WORK

--- src/Dom.cpp
+++ src/Dom.cpp
@@ -7,6 +7,7 @@
 #include "llvm/IR/CFG.h"
 
 #include <cstddef>
+#include <llvm/ADT/DenseMap.h>
 #include <map>
 #include <vector>
 #include <set>
@@ -25,15 +26,17 @@ namespace {
         ) {
 
         int timer = 0; 
-        std::map<const llvm::BasicBlock*, std::size_t> dfsNum;
+        llvm::DenseMap<const llvm::BasicBlock*, std::size_t> dfsNum;
         std::vector<const llvm::BasicBlock*> dfsOrder; 
         std::vector<std::size_t> label; 
         std::vector<std::size_t> sdom; 
         std::vector<std::size_t> disjoint_set_union; 
         std::vector<std::size_t> dfs_parent;
         std::vector<std::size_t> reverse_graph;
-        std::vector<std::vector<const llvm::BasicBlock*>> bucket;
         std::vector<std::size_t> dom;
+        std::vector<std::size_t> par;
+        std::vector<std::vector<int>> tree;
+        std::vector<std::vector<int>> bucket;
 
         // VI g[N],tree[N],rg[N],bucket[N];
         // int sdom[N],par[N],dom[N],dsu[N],label[N];
@@ -43,28 +46,29 @@ namespace {
         {
           const llvm::BasicBlock *BB = dfsOrder[i];
 
-          for(auto Pred: llvm::predecessors(BB))
-            sdom[i] = std::min(sdom[i], sdom[Find(disjoint_set_union, label, sdom, Pred, 0)]);
+          for(const llvm::BasicBlock* Pred: llvm::predecessors(BB))
+            sdom[i] = std::min(sdom[i], sdom[Find(dfsNum, dfsOrder, disjoint_set_union, label, sdom, Pred, 0)]);
 
           if(i > 1)
-            bucket[sdom[i]].push_back(BB);
+            bucket[sdom[i]].push_back(dfsNum[BB]);
 
           for(auto bucket_BB: bucket[i])
           {
-            auto v = Find(disjoint_set_union, label, sdom, bucket_BB, 0);
-            if(sdom[v]==sdom[bucket_BB]) 
+            auto v = Find(dfsNum, dfsOrder, disjoint_set_union, label, sdom, dfsOrder[bucket_BB], 0);
+            if(sdom[dfsNum[v]] == sdom[bucket_BB]) 
               dom[bucket_BB]=sdom[bucket_BB];
             else 
-              dom[bucket_BB] = v;
+              dom[bucket_BB] = dfsNum[v];
           }
-          if(i>1)Union(par[i],i);
+          if(i > 1)
+            Union(disjoint_set_union, par[i] ,i);
         }
 
-        for(int i=2;i<=n;i++)
+        for(int i = 2; i <= n; i++)
         {
-          if(dom[i]!=sdom[i])dom[i]=dom[dom[i]];
-          tree[rev[i]].PB(rev[dom[i]]);
-          tree[rev[dom[i]]].PB(rev[i]);
+          if(dom[i] != sdom[i])dom[i] = dom[dom[i]];
+          tree[dfsNum[dfsOrder[i]]].push_back(dfsNum[dfsOrder[dom[i]]]);
+          tree[dfsNum[dfsOrder[dom[i]]]].push_back(dfsNum[dfsOrder[i]]);
         }
 
         return llvm::PreservedAnalyses::all();
@@ -73,8 +77,8 @@ namespace {
     void dfs0(
         const llvm::BasicBlock* u, 
         int timer, 
-        std::map<const llvm::BasicBlock*, std::size_t> arrival, 
-        std::vector<const llvm::BasicBlock*> reverse_arrival, 
+        llvm::DenseMap<const llvm::BasicBlock*, std::size_t> dfsNum, 
+        std::vector<const llvm::BasicBlock*> dfsOrder, 
         std::vector<std::size_t> label, 
         std::vector<std::size_t> sdom, 
         std::vector<std::size_t> disjoint_set_union, 
@@ -82,8 +86,8 @@ namespace {
         )
     {
       timer++;
-      arrival[u]=timer;
-      reverse_arrival[timer]=u;
+      dfsNum[u]=timer;
+      dfsOrder[timer]=u;
 
       label[timer]=timer;
       sdom[timer]=timer;
@@ -91,29 +95,32 @@ namespace {
 
       for(const llvm::BasicBlock* Succ: llvm::successors(u))
       {
-        if(!arrival[Succ])
+        if(!dfsNum[Succ])
         {
-          dfs0(Succ, timer, arrival, reverse_arrival, label, sdom, disjoint_set_union, dfs_parent);
-          dfs_parent[arrival[Succ]]=arrival[u];
+          dfs0(Succ, timer, dfsNum, dfsOrder, label, sdom, disjoint_set_union, dfs_parent);
+          dfs_parent[dfsNum[Succ]]=dfsNum[u];
         }
       }
     }
 
-    int Find(
+    const llvm::BasicBlock* Find(
+        llvm::DenseMap<const llvm::BasicBlock*, std::size_t> dfsNum,
+        std::vector<const llvm::BasicBlock*> dfsOrder, 
         std::vector<size_t> dsu,
         std::vector<size_t> label,
         std::vector<size_t> sdom,
         const llvm::BasicBlock* u, int x=0
         )
     {
-      if(u==dsu[u]) return (x)?-1 : u;
-      int v = Find(dsu[u], x+1);
-      if(v<0) return u;
+      auto dfsNum_u = dfsNum[u];
+      if(dfsNum_u==dsu[dfsNum_u]) return (x)? NULL : u;
+      const llvm::BasicBlock* v = Find(dfsNum, dfsOrder, dsu, label, sdom, dfsOrder[dfsNum_u], x+1);
+      if(v==NULL) return u;
 
-      if(sdom[label[dsu[u]]] < sdom[label[u]])
-        label[u] = label[dsu[u]];
-      dsu[u] = v;
-      return (x)?v:label[u];
+      if(sdom[label[dsu[dfsNum_u]]] < sdom[label[dfsNum_u]])
+        label[dfsNum_u] = label[dsu[dfsNum_u]];
+      dsu[dfsNum_u] = dfsNum[v];
+      return (x ? v: dfsOrder[label[dfsNum_u]]);
     }
 
     void Union(std::vector<size_t> dsu, int u, int v){ //Add an edge u-->v
